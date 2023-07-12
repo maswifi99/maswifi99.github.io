89b3182fc52bf26aa21469e23bdd03dce6e8134e
maswifi99 <139038373+maswifi99@users.noreply.github.com>
2023-07-12 09:29:41 +0800

    Add files via upload

---
+++ myqr/js/style.js
@@ -0,0 +1,10 @@
+
+function opcam(){
+	var x = document.getElementById('qr-input-file').click();
+	document.getElementById("qr-input-file").innerHTML = x;
+}
+
+function file(){
+	var x = document.getElementById('qr-input-file').click();
+	document.getElementById("qr-input-file").innerHTML = x;
+}
